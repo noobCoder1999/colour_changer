b437a5c201c3747573ac315ccf957d63f1ac0fce
Raunak <sharmaraunak338@gmail.com>
2020-07-29 13:19:40 +0530

    app logic completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useEffect,useState}from 'react';
 
 import './App.css';
+import Recipe from './recipe';
 
 
 const App = () =>{
@@ -8,15 +9,32 @@ const App = () =>{
   const APP_ID ="418721ce";
   const APP_KEY ="1732b93e9f3d03102e681acfd5a391b0";
 
+  const [recipes,setRecipe] = useState([]);
+  const [search,setSearch] = useState("");
+  const [query,setQuery] = useState("chicken");
+
   useEffect(() =>{
     getRecipe();
-  },[]);
+  },[query]);
 
   const getRecipe = async () =>{
 
-    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data);
+    setRecipe(data.hits);
+  }
+
+  const updateSearch = e =>{
+    setSearch(e.target.value);
+   console.log(search);
+
+  }
+
+  const getSearch = e =>{
+    e.preventDefault();
+    setQuery(search);
+   
+    setSearch("");
   }
 
 
@@ -24,11 +42,20 @@ const App = () =>{
   return(
 
     <div className ="App">
-      <form action="" className="search-form">
-        <input type="text" className="search-bar"/>
-        <button className="search-button" type ="submit">Search</button>
+      <form  onSubmit = {getSearch} className="search-form">
+        <input type="text" className="search-bar" value = {search} onChange = {updateSearch}/>
+        <button className="search-button" type ="submit" >Search</button>
       </form>
-
+      <div className="recipies">
+      {recipes.map(recipe => (
+        <Recipe 
+          key = {recipe.recipe.label}
+          title={recipe.recipe.label} 
+          calories = {recipe.recipe.calories} 
+          image={recipe.recipe.image}
+          ingredients = {recipe.recipe.ingredients}/>
+      ))}
+  </div>
     </div>
 
   );
